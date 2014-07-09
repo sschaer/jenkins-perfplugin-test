f38fcb4998c76d2c18a1ac9af479711a895b7e7f
sschaer <sebastian.schaer@gmx.de>
2014-07-09 17:00:07 +0200

    adding some thread sleep

--- src/com/arvatosystems/us/helloworld/HelloWorld.java
+++ src/com/arvatosystems/us/helloworld/HelloWorld.java
@@ -1,7 +1,21 @@
 package com.arvatosystems.us.helloworld;
 
+import java.util.Random;
+
 public class HelloWorld {
-	  public static void main(String[] args) {
-		    System.out.println("Hello World");
-	  }
+	public static void main(String[] args) {
+		Random r = new Random();
+		long sleep = (long) r.nextInt(10000);
+		System.out.println("sleeping for " + sleep + " milliseconds");
+		try {
+			Thread.sleep(sleep);
+		} catch (InterruptedException e) {
+			System.out.println("'knock knock'");
+			System.out.println("'who's there?'");
+			System.out.println("'interupting cow!");
+			System.out.println("'interrupting wh..'");
+			System.out.println("'MOOOOOOOOOOOOOO!!!'");
+		}
+		System.out.println("woke up!");
+	}
 }
